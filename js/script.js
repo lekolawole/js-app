1fe3abca6f15bd07e40f883cc472c8c98d793a1e
lekolawole <lekolawole@gmail.com>
2022-03-04 18:11:22 -0500

    Entered pokemonList array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-alert('Hello world');
-
-favoriteFood = 'Baja Tacos';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Charmander', height: 0.6, type:['monster', 'dragon']},
+    {name: 'Geodude', height: 0.4, type:['mineral']},
+    {name: 'Grotle', height: 1.1, type:['monster', 'grass']},
+    {name: 'Unfezant', height: 1.2, type:['flying']},
+    {name: 'Zoroark', height: 1.6, type:['field']},
+    {name: 'Pikachu', height: 0.4, type:['field', 'fairy']},
+    {name: 'Wartortle', height: 1, type:['monster', 'water']},
+];
