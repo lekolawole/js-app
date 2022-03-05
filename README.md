44b8c725670e739e853acaa7a04c91dc066703f0
lekolawole <lekolawole@gmail.com>
2022-03-05 16:06:00 -0500

    Task 1.3: Loops, Arrays, & Conditions
    Added for loop including height conditions, name, and a message.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # js-app
  
+// A directory of Pokemon with stats //
+
+pokemonInfo = printed name and height of each pokemon in the array; 
+
--- js/script.js
+++ js/script.js
@@ -1,9 +1,49 @@
 let pokemonList = [
-    {name: 'Charmander', height: 0.6, type:['monster', 'dragon']},
-    {name: 'Geodude', height: 0.4, type:['mineral']},
-    {name: 'Grotle', height: 1.1, type:['monster', 'grass']},
-    {name: 'Unfezant', height: 1.2, type:['flying']},
-    {name: 'Zoroark', height: 1.6, type:['field']},
-    {name: 'Pikachu', height: 0.4, type:['field', 'fairy']},
-    {name: 'Wartortle', height: 1, type:['monster', 'water']},
+    {
+        name: 'Charmander', 
+        height: 0.6, 
+        type:['monster', 'dragon']
+    },
+    {
+        name: 'Geodude', 
+        height: 0.4, 
+        type:['mineral']
+    },
+    {
+        name: 'Grotle', 
+        height: 1.1, 
+        type:['monster', 'grass']
+    },
+    {
+        name: 'Unfezant', 
+        height: 1.2, 
+        type:['flying']
+    },
+    {
+        name: 'Zoroark', 
+        height: 1.6, 
+        type:['field']
+    },
+    {
+        name: 'Pikachu', 
+        height: 0.4, 
+        type:['field', 'fairy']
+    },
+    {
+        name: 'Wartortle', 
+        height: 1, 
+        type:['monster', 'water']
+    },
 ];
+
+ 
+let pokemonInfo = ' (height: \n';
+for (let i = 1; i < pokemonList.length; i++) {
+
+    if (pokemonList[i].height > 1) {
+        document.write(pokemonList[i].name + `${pokemonInfo} ${pokemonList[i].height}` + ') ' + '- Wow, that\'s a big Pokemon!')
+    } else {
+        document.write(pokemonList[i].name + `${pokemonInfo} ${pokemonList[i].height}` + ') ')
+    }
+}
+
