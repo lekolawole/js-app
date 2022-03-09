4f654334b9dd0672e97edcaca6e729f24c317c72
lekolawole <lekolawole@gmail.com>
2022-03-08 19:24:40 -0500

    Updated forEach loop with Message
    Included height conditions for previous for loop with a message.

--- js/script.js
+++ js/script.js
@@ -1,79 +1,3 @@
-let pokemonList = [
-    {
-        name: 'Charmander', 
-        height: 0.6, 
-        type:['monster', 'dragon']
-    },
-    {
-        name: 'Geodude', 
-        height: 0.4, 
-        type:['mineral']
-    },
-    {
-        name: 'Grotle', 
-        height: 1.1, 
-        type:['monster', 'grass']
-    },
-    {
-        name: 'Unfezant', 
-        height: 1.2, 
-        type:['flying']
-    },
-    {
-        name: 'Zoroark', 
-        height: 1.6, 
-        type:['field']
-    },
-    {
-        name: 'Pikachu', 
-        height: 0.4, 
-        type:['field', 'fairy']
-    },
-    {
-        name: 'Wartortle', 
-        height: 1, 
-        type:['monster', 'water']
-    },
-];
-
-let pokemonList2 = [
-    {
-        name: 'Blastoise', 
-        height: 1.6, 
-        type:['monster', 'water']
-    },
-    {
-        name: 'Spearow', 
-        height: 0.3, 
-        type:['flying']
-    },
-    {
-        name: 'Sandslash', 
-        height: 1.1, 
-        type:['field']
-    },
-    {
-        name: 'Grimer', 
-        height: 0.9, 
-        type:['amorphous']
-    },
-    {
-        name: 'Staryu', 
-        height: 0.8, 
-        type:['water']
-    },
-    {
-        name: 'Quilava', 
-        height: 0.9, 
-        type:['field']
-    },
-    {
-        name: 'Umbreon', 
-        height: 1, 
-        type:['field']
-    },
-];
-
 let bigPokemon = '- Wow, that\'s a big Pokemon!';
 
 /* ///////////for-loop task 
@@ -92,13 +16,61 @@ printPokemonListDetails(pokemonList);
 printPokemonListDetails(pokemonList2);
 */
 
+// Creating the IIFE
+
+let pokemonRepository = (function(list) {
+    let pokemonList = [
+        {name: 'Charmander', height: 0.6, type:['monster', 'dragon']},
+
+        {name: 'Geodude', height: 0.4, type:['mineral']},
+    
+        { name: 'Grotle', height: 1.1, type:['monster', 'grass']},
+   
+        {name: 'Unfezant', height: 1.2, type:['flying']},
+    
+        {name: 'Zoroark', height: 1.6, type:['field']},
+    
+        {name: 'Pikachu', height: 0.4, type:['field', 'fairy']},
+    
+        {name: 'Wartortle', height: 1, type:['monster', 'water']},
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+
+})();
+
+// (getAll) and (add) functions
+
+pokemonRepository.getAll().forEach(function(list){
+    if (list.height > 1.2) { //prints a message for biggest Pokemon
+        document.write(`${list.name} (height: ${list.height}) ${bigPokemon}<br>`)
+    } else {
+        document.write(`${list.name} (height: ${list.height})<br>`)
+    }
+});
+
+pokemonRepository.add({name: 'Blastoise'});
 
 // forEach task 
 
-pokemonList.forEach(function(list){
+/*pokemonList.forEach(function(list){
             document.write(`${list.name} (height: ${list.height})<br>`);
 });
 
 pokemonList2.forEach(function(list){
     document.write(`${list.name} (height: ${list.height})<br>`);
 });
+*/
+
+
