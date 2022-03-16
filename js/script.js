d6825053bc7525005e09d7d4631678084a51c335
lekolawole <lekolawole@gmail.com>
2022-03-15 21:18:13 -0400

    First update adding API

--- js/script.js
+++ js/script.js
@@ -1,6 +1,121 @@
 // Creating the IIFE
 
-let pokemonRepository = (function(list) {
+let pokemonRepository = (function () {
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    let pokemonList = [];
+
+    // adds pokemon to list
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    // Creating addListItem function & buttons
+    function addListItem(list) {
+
+        let listElement = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = list.name;
+
+        button.classList.add('pokemon-button');
+        listItem.append(button);
+        listElement.append(listItem);
+
+        button.addEventListener('click', () => {
+            showDetails(list);
+        });
+    }
+
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+                console.log(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
+
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+            // Now we add the details to the item
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(function (e) {
+            console.error(e);
+        });
+    }
+
+    // Creating Event Listeners
+    function showDetails(item) {
+        loadDetails(item).then(function () {
+            console.log(item);
+        });
+    }
+
+
+    return {
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails
+    };
+
+})();
+
+pokemonRepository.loadList().then(function () {
+    // Now the data is loaded!
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
+});
+
+// Adding more Pokemon
+
+pokemonRepository.add({name: 'Blastoise', height: 1.2, type:['monster', 'water']});
+console.log(pokemonRepository.getAll());
+
+
+// (getAll) and (add) functions
+
+let bigPokemon = '- Wow, that\'s a big Pokemon!';
+
+pokemonRepository.getAll().forEach(function(pokemon){
+   pokemonRepository.addListItem(pokemon)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let pokemonRepository = (function(list) {
     let pokemonList = [
         {name: 'Charmander', height: 0.6, type:['monster', 'dragon']},
 
@@ -56,20 +171,4 @@ let pokemonRepository = (function(list) {
         addListItem: addListItem
     };
 
-})();
-
-// Adding more Pokemon
-
-pokemonRepository.add({name: 'Blastoise', height: 1.2, type:['monster', 'water']});
-console.log(pokemonRepository.getAll());
-
-
-// (getAll) and (add) functions
-
-let bigPokemon = '- Wow, that\'s a big Pokemon!';
-
-pokemonRepository.getAll().forEach(function(list){
-   pokemonRepository.addListItem(list)
-});
-
-
+})();*/
