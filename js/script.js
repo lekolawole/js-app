531847e86ab15e6b3dcf135fa5123dd87e43d27c
lekolawole <lekolawole@gmail.com>
2022-03-17 19:51:06 -0400

    Update CSS and HTML
    - Added color scheme to CSS
    - Removed unnecessary code from JS and HTML
    - Added notes for understanding

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ let pokemonRepository = (function () {
         }).then(function (json) {
             json.results.forEach(function (item) {
                 let pokemon = {
-                    name: item.name,
+                    name: item.name.toUpperCase(),
                     detailsUrl: item.url
                 };
                 add(pokemon);
@@ -53,12 +53,12 @@ let pokemonRepository = (function () {
 
     // toggles loading messages with class-lists
     function toggleLoadingMessage(item) {
-        loadingMessage.classList.add("show")
+        loadingMessage.classList.add('show')
     }
 
     function toggleLoadingMessageOff(item) {
         setTimeout(function () {
-            loadingMessage.classList.remove("show");
+            loadingMessage.classList.remove('show');
         }, 500);
     }
 
@@ -164,78 +164,3 @@ pokemonRepository.getAll().forEach(function(pokemon){
    pokemonRepository.addListItem(pokemon)
 });
 // END OF pokemonRepository ------------------------------------------// 
-
-
-// IIFE - UI Tasks - Real Time Validation Forms 
-
-(function () {
-    let form = document.querySelector('#register-form');
-    let emailInput = document.querySelector('#email');
-    let passwordInput = document.querySelector('#password');
-
-    function showErrorMessage(input, message) {
-        let container = input.parentElement;
-        let error = container.querySelector('.error-message');
-        if (error) {
-            container.removeChild(error);
-        }
-            // Creates error messages as user starts typing 
-        if (message) {
-            let error = document.createElement('div');
-            error.classList.add('error-message');
-            error.innerText = message;
-            container.appendChild(error);
-        }
-    }
-
-    function validateEmail() {
-        let value = emailInput.value;
-
-        if (!value) {
-            showErrorMessage(emailInput, 'E-mail is a required field.');
-            return false;
-        }
-        if (value.indexOf('@') === -1) {
-            showErrorMessage(emailInput, 'You must enter a valid email address.');
-            return false;
-        }
-
-        showErrorMessage(emailInput, null);
-        return true;
-    }
-
-    function validatePassword() {
-        let value = passwordInput.value;
-
-        if (!value) {
-            showErrorMessage(passwordInput, 'Password is a required field.');
-            return false;
-        }
-        if (value.length < 8) {
-            showErrorMessage(passwordInput, 'The password needs to be at least 8 characters long.');
-            return false;
-        }
-
-        showErrorMessage(passwordInput, null);
-        return true;
-    }
-
-
-    function validateForm() {
-        let isValidEmail = validateEmail();
-        let isValidPassword = validatePassword();
-        return isValidEmail && isValidPassword;
-    }
-
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        if (validateForm()) {
-            alert('Success!🚀');
-        }
-    })
-    // Adding event listeners to forms within the whole function
-    emailInput.addEventListener('input', validateEmail);
-    passwordInput.addEventListener('input', validatePassword);
-})();
-
-// ------------------- end of form validation IIFE --------------------//
