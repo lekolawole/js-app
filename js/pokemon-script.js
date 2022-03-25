2c30123c8e99c6b5e5daee9a1131ce7b29c80ad4
lekolawole <lekolawole@gmail.com>
2022-03-25 15:07:59 -0400

    Update CSS, Removed unused code

--- js/pokemon-script.js
+++ js/pokemon-script.js
@@ -2,7 +2,6 @@
 
 let pokemonRepository = (function () {
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-    //let modalContainer = document.querySelector('#modal-container');
     let pokemonList = [];
 
     // adds pokemon to list
@@ -26,11 +25,11 @@ let pokemonRepository = (function () {
 
         pokemonButton.setAttribute('data-toggle', 'modal');
         pokemonButton.setAttribute('data-target', '#pokemonModal');
-        pokemonButton.classList.add('pokemon-button', 'btn', 'btn-primary');
+        pokemonButton.classList.add('pokemon-button', 'btn');
         listItem.append(pokemonButton);
         listElement.append(listItem);
 
-        pokemonButton.addEventListener('click', () => {
+        pokemonButton.addEventListener('click', function() {
             showDetails(list);
         });
     }
@@ -53,20 +52,6 @@ let pokemonRepository = (function () {
         })
     }
 
-
-    //let loadingMessage = document.querySelector('.message');
-    /*
-        // toggles loading messages with class-lists
-        function toggleLoadingMessage(item) {
-            loadingMessage.classList.add('show')
-        }
-    
-        function toggleLoadingMessageOff(item) {
-            setTimeout(function () {
-                loadingMessage.classList.remove('show');
-            }, 500);
-        }*/
-
     // loads details of Pokemon when clicked
     function loadDetails(item) {
         let url = item.detailsUrl;
@@ -76,7 +61,10 @@ let pokemonRepository = (function () {
             // Now we add the details to the item
             item.imageUrl = details.sprites.front_default;
             item.height = `Height: ${details.height}`;
-            item.types = details.types;
+            item.types = []; 
+            for (let i = 0; i< details.types.length; i++) {
+                item.types.push(details.types[i].type.name);
+            }
         }).catch(function (e) {
             console.error(e);
         });
@@ -85,62 +73,14 @@ let pokemonRepository = (function () {
     // Creating Event Listeners on pokemon info divs
     function showDetails(item) {
         loadDetails(item).then(function () {
-            // add in code for modal 
-            //findImageUrl(item); //calls function to hold url for modal later
+            // add in code to show modal 
+            
             console.log(item);
             showModal(item);
         });
     }
-    // // Creates new URL for pokemon image based on clicked Pokemon
-    // function findImageUrl(item) {
-    //     let newImageUrl = item.imageUrl;
-    //     console.log(newImageUrl);
-    //     /*showModal(item.name, newImageUrl, item.height);*/
-    // }
-
-    /*// Functions for hiding/showing modal containing Pokemon details 
-    function hideModal() {
-        let modalContainer = document.querySelector('#modal-container');
-        modalContainer.classList.remove('is-visible');
-    }*/
 
     function showModal(item) {
-        /*//displays the modal after .5s for a consistent UI experience
-        setTimeout(function () {
-            modalContainer.classList.add('is-visible');
-        }, 500);
-
-        let modalContainer = document.querySelector('#modal-container');
-        modalContainer.innerHTML = '';
-
-        // Creating modal elemens in HTML
-        let modal = document.createElement('div');
-        modal.classList.add('modal');
-
-        let closeButtonElement = document.createElement('button');
-        closeButtonElement.classList.add('modal-close');
-        closeButtonElement.innerText = 'Close';
-        closeButtonElement.addEventListener('click', hideModal);
-
-
-        let pokemonTitleElement = document.createElement('h1');
-        pokemonTitleElement.innerText = title;
-
-        /*let pokemonImg = document.createElement('img');
-        pokemonImg.src = `${newImageUrl}`; //included backtik(`)- make url in src
-        pokemonImg.classList.add('pokemon-modal-img');// don't need to include '.'
-
-        let contentElement = document.createElement('p');
-        contentElement.innerText = text;
-
-        modal.appendChild(closeButtonElement);
-        modal.appendChild(pokemonTitleElement);
-        modal.appendChild(pokemonImg);
-        modal.appendChild(contentElement);
-        modalContainer.appendChild(modal);
-
-        modalContainer.addEventListener('click', hideModal);*/
-
         let modalBody = $('.modal-body');
         let modalTitle = $('.modal-title');
         let modalHeader = $('modalHeader');
@@ -151,8 +91,11 @@ let pokemonRepository = (function () {
         //filling Modal with pokemon info
 
         let pokemonName = $("<h1>" + item.name + "</h1>");
-        let pokemonImg = $('<img class=\'pokemon-image\'>');
-        pokemonImg.attr("src", item.imageUrl);
+        let pokemonImg = $('<img class=\'pokemon-image\'>');//Creates img element
+        pokemonImg.attr("src", item.imageUrl);//Updates image source 
+        pokemonImg.addClass('rounded-circle border border-dark');
+        // pokemonImg.addClass('border');
+        // pokemonImg.addClass('border-primary');
         let pokemonHeight = $("<p>" + item.height + "</p>");
         let pokemonTypes = $("<p>" + item.types + "</p>");
 
@@ -161,27 +104,11 @@ let pokemonRepository = (function () {
         modalBody.append(pokemonImg);
         modalBody.append(pokemonHeight);
         modalBody.append(pokemonTypes);
-
-        //toggleLoadingMessageOff()// removes loading message after API is loaded
     }
 
-    /*// Esc key functionality 
-    let modal = document.createElement('div');
-    modal.classList.add('modal');
+   
 
-    window.addEventListener('keydown', (e) => {
 
-        if (e.key === 'Escape') {
-            hideModal();
-        }
-    });
-
-    modalContainer.addEventListener('click', (e) => {
-        let target = e.target;
-        if (target === modalContainer) {
-            hideModal();
-        }
-    });*/
 
     return {
         add: add,
